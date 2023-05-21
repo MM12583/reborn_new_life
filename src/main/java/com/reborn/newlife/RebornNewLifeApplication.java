7b7e9fde7de82b2d3599a69be9df5c963ac71cd8
FrankHsiao <lordarcher12345@gmail.com>
2023-05-21 13:32:25 +0800

    security basic unused & add mockito for unitTest

--- src/main/java/com/reborn/newlife/RebornNewLifeApplication.java
+++ src/main/java/com/reborn/newlife/RebornNewLifeApplication.java
@@ -2,11 +2,10 @@ package com.reborn.newlife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @EnableJdbcHttpSession // 代替原始session
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // 先排除security
+@SpringBootApplication 
 public class RebornNewLifeApplication {
 	
     public static void main(String[] args) {
